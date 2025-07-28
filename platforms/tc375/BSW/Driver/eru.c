bbe77e365bc5d33df7b505f1d4af96e097a69554
HJ <ihjkong@naver.com>
2025-07-28 11:01:01 +0900

    [SMK-25] / 초음파센서 input 구현

--- platforms/tc375/BSW/Driver/eru.c
+++ platforms/tc375/BSW/Driver/eru.c
@@ -1,3 +1,4 @@
+//eru.c
 /*
  * scueru.c
  *
@@ -9,38 +10,125 @@
 #include <Ifx_Types.h>
 #include <IfxScuWdt.h>
 #include "my_stdio.h"
+#include "eru.h"
 
-void scueru_Init()
+void scueru_Init(void)
 {
     uint16 password = IfxScuWdt_getSafetyWatchdogPasswordInline();
     IfxScuWdt_clearSafetyEndinitInline(password);
+    MODULE_P13.IOCR0.B.PC2 = 0x10; // P13.2 - Rear TRIG
+    MODULE_P15.IOCR0.B.PC3 = 0x10; // P15.3 - Right TRIG
+    MODULE_P10.IOCR4.B.PC7 = 0x10; // P10.7 - Left TRIG
 
-    MODULE_P02.IOCR0.B.PC1 = 0x02; /* Set P2.1 as pull-up input */
+    /*** GPIO 입력 설정 (Echo 핀들) ***/
+    MODULE_P13.IOCR0.B.PC1 = 0x02; // P13.1 - Rear sensor echo
+    MODULE_P15.IOCR0.B.PC2 = 0x02; // P15.2 - Right sensor echo
+    MODULE_P02.IOCR8.B.PC8 = 0x02; // P02.8 - Left sensor echo
 
-    /* EICR.EXIS 레지스터 설정 : ESR2, 1번 신호 */
-    MODULE_SCU.EICR[1].B.EXIS0 = 1;
-    /* rising, falling edge 트리거 설정 */
-    MODULE_SCU.EICR[1].B.REN0 = 0;
-    MODULE_SCU.EICR[1].B.FEN0 = 1;
-    /* Enable Trigger Pulse */
+    /*** Rear Sensor (P13.1 → EICR[0].Channel0) ***/
+    MODULE_SCU.EICR[0].B.EXIS0 = 2;     // EXIS3 → P13.1
+    MODULE_SCU.EICR[0].B.REN0  = 1;     // Rising edge enable
+    MODULE_SCU.EICR[0].B.FEN0  = 1;     // Falling edge enable
+    MODULE_SCU.EICR[0].B.EIEN0 = 1;     // Enable request
+    MODULE_SCU.EICR[0].B.INP0  = 0;     // Connect to SRN0
+    MODULE_SCU.IGCR[0].B.IGP0  = 1;     // Pattern match mode
+    MODULE_SRC.SCU.SCUERU[0].B.SRPN = ISR_PRIORITY_ULTRASONIC_REAR;
+    MODULE_SRC.SCU.SCUERU[0].B.TOS  = 0;  // CPU0
+    MODULE_SRC.SCU.SCUERU[0].B.CLRR = 1;
+    MODULE_SRC.SCU.SCUERU[0].B.SRE  = 1;
+
+    /*** Right Sensor (P15.2 → EICR[0].Channel1) ***/
+    MODULE_SCU.EICR[0].B.EXIS1 = 1;     // EXIS1 → P15.2
+    MODULE_SCU.EICR[0].B.REN1  = 1;
+    MODULE_SCU.EICR[0].B.FEN1  = 1;
+    MODULE_SCU.EICR[0].B.EIEN1 = 1;
+    MODULE_SCU.EICR[0].B.INP1  = 1;     // Connect to SRN1
+    MODULE_SCU.IGCR[0].B.IGP1  = 1;
+    MODULE_SRC.SCU.SCUERU[1].B.SRPN = ISR_PRIORITY_ULTRASONIC_RIGHT;
+    MODULE_SRC.SCU.SCUERU[1].B.TOS  = 0;
+    MODULE_SRC.SCU.SCUERU[1].B.CLRR = 1;
+    MODULE_SRC.SCU.SCUERU[1].B.SRE  = 1;
+
+    /*** Left Sensor (P02.8 → EICR[1].Channel0) ***/
+    MODULE_SCU.EICR[1].B.EXIS0 = 2;     // EXIS2 → P02.8
+    MODULE_SCU.EICR[1].B.REN0  = 1;
+    MODULE_SCU.EICR[1].B.FEN0  = 1;
     MODULE_SCU.EICR[1].B.EIEN0 = 1;
-    /* Determination of output channel for trigger event (Register INP) */
-    MODULE_SCU.EICR[1].B.INP1 = 0;
-    /* Configure Output channels, outputgating unit OGU (Register IGPy) */
-    MODULE_SCU.IGCR[0].B.IGP0 = 1;
-
-    volatile Ifx_SRC_SRCR *src;
-    src = (volatile Ifx_SRC_SRCR*) (&MODULE_SRC.SCU.SCUERU);
-    src->B.SRPN = ISR_PRIORITY_ERU_INT0;
-    src->B.TOS = 0;
-    src->B.CLRR = 1; /* clear request */
-    src->B.SRE = 1; /* interrupt enable */
-
+    MODULE_SCU.EICR[1].B.INP0  = 2;     // Connect to SRN2
+    MODULE_SCU.IGCR[1].B.IGP0  = 1;
+    MODULE_SRC.SCU.SCUERU[2].B.SRPN = ISR_PRIORITY_ULTRASONIC_LEFT;
+    MODULE_SRC.SCU.SCUERU[2].B.TOS  = 0;
+    MODULE_SRC.SCU.SCUERU[2].B.CLRR = 1;
+    MODULE_SRC.SCU.SCUERU[2].B.SRE  = 1;
     IfxScuWdt_setSafetyEndinitInline(password);
+
+//    rear_sensor.start_time = 0; //////////////////////////
 }
 
-IFX_INTERRUPT(SCUERU_Int0_Handler, 0, ISR_PRIORITY_ERU_INT0);
-void SCUERU_Int0_Handler (void)
-{
-    my_printf("SCU_ERU_INT0!\n");
+IFX_INTERRUPT(SCUERU_Int0_Handler, 0,ISR_PRIORITY_ULTRASONIC_REAR);
+void SCUERU_Int0_Handler(void) {
+    uint64 now = getTimeUs();
+    my_printf("hanler 0 on");
+
+//    if (P13_IN.B.P1 == 1) {  // Rising edge
+//        rear_sensor.start_time = now;
+//        my_printf("\nSTART %llu\n",rear_sensor.start_time);
+//        rear_sensor.echo_state = 1;
+//    } else {  // Falling edge
+//        rear_sensor.end_time = now;
+//        my_printf("\nEND %llu\n",rear_sensor.end_time);
+//        my_printf("\n%llu %llu\n",rear_sensor.start_time,rear_sensor.end_time);
+//        rear_sensor.duration = rear_sensor.end_time - rear_sensor.start_time;
+//        rear_sensor.distance_cm = (0.0343f * rear_sensor.duration) / 2.0f;
+//        rear_sensor.echo_state = 0;
+//    }
+
+
+    // 인터럽트 핸들러 내부
+    if (rear_sensor.echo_state == 0) {
+        // Rising edge
+        rear_sensor.start_time = getTimeUs();
+        rear_sensor.echo_state = 1;
+        my_printf("START %llu\n", rear_sensor.start_time);
+    } else {
+        // Falling edge
+        rear_sensor.end_time = getTimeUs();
+        rear_sensor.duration = rear_sensor.end_time - rear_sensor.start_time;
+        rear_sensor.distance_cm = (0.0343f * rear_sensor.duration) / 2.0f;
+        rear_sensor.echo_state = 0;
+        my_printf("END %llu\n", rear_sensor.end_time);
+    }
+
+
+}
+
+
+IFX_INTERRUPT(SCUERU_Int1_Handler, 0, ISR_PRIORITY_ULTRASONIC_RIGHT);
+void SCUERU_Int1_Handler(void) {
+    uint64 now = getTimeUs();
+    if (P15_IN.B.P2 == 1) {
+        right_sensor.start_time = now;
+        right_sensor.echo_state = 1;
+    } else {
+        right_sensor.end_time = now;
+        right_sensor.duration = right_sensor.end_time - right_sensor.start_time;
+        right_sensor.distance_cm = (0.0343f * right_sensor.duration) / 2.0f;
+        right_sensor.echo_state = 0;
+    }
+    my_printf("hanler 1 on");
+}
+
+IFX_INTERRUPT(SCUERU_Int2_Handler, 0,ISR_PRIORITY_ULTRASONIC_LEFT);
+void SCUERU_Int2_Handler(void) {
+    uint64 now = getTimeUs();
+    if (P02_IN.B.P8 == 1) {
+        left_sensor.start_time = now;
+        left_sensor.echo_state = 1;
+    } else {
+        left_sensor.end_time = now;
+        left_sensor.duration = left_sensor.end_time - left_sensor.start_time;
+        left_sensor.distance_cm = (0.0343f * left_sensor.duration) / 2.0f;
+        left_sensor.echo_state = 0;
+    }
+    my_printf("hanler 2 on");
 }
--- platforms/tc375/BSW/Driver/eru.h
+++ platforms/tc375/BSW/Driver/eru.h
@@ -1,13 +1,36 @@
+//eru.h
 /*
  * scueru.h
  *
  *  Created on: 2024. 11. 6.
  *      Author: Kimjh
  */
-
 #ifndef BSW_DRIVER_ERU_H_
 #define BSW_DRIVER_ERU_H_
-
 void scueru_Init(void);
+void SCUERU_Int0_Handler(void);
+void SCUERU_Int1_Handler(void);
+void SCUERU_Int2_Handler(void);
+typedef struct {
+    uint64 start_time;
+    uint64 end_time;
+    uint64 duration;
+    float  distance_cm;
+    uint8  echo_state;
+} UltrasonicSensor;
+typedef struct {
+    uint64 rear_sensor_start_time;
+    uint64 rear_sensor_end_time;
+    uint64 left_sensor_start_time;
+    uint64 left_sensor_end_time;
+    uint64 right_sensor_start_time;
+    uint64 right_sensor_end_time;
+} UltrasonicSensor_Info;
+
+// 전역 변수 선언 (extern 키워드 사용)
+volatile UltrasonicSensor rear_sensor;
+volatile UltrasonicSensor right_sensor;
+volatile UltrasonicSensor left_sensor;
+volatile UltrasonicSensor_Info;
 
 #endif /* BSW_DRIVER_ERU_H_ */
