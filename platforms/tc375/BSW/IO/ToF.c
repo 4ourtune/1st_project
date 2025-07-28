021cdffd74b1a98bc6930a718f8ed90a76e13ca0
dltmdwn0325 <dltmdwn0325@gmail.com>
2025-07-28 11:13:33 +0900

    [SMK-24] / ToF-input
    1. Can.c에서 Can_RX 인터럽트 받는거 주석 처리
    2. ToF.c에서 데이터 무결성을 보장하면서 거리 데이터 받게 수정
    3. ToF.c에서 거리 보정(5cm) 함수 추가

--- platforms/tc375/BSW/IO/ToF.c
+++ platforms/tc375/BSW/IO/ToF.c
@@ -2,28 +2,47 @@
 #include "my_stdio.h"
 #include "GPIO.h"
 #include "can.h"
+#include "stm.h"
 
 static unsigned int g_TofValue = 0;
+static unsigned short g_SignalStrength = 0;
+static unsigned char g_DisStatus = 0;
+static uint64 g_TofTimestampUs = 0;
 
-//IFX_INTERRUPT(TofIsrHandler, 0, ISR_PRIORITY_CAN_RX);
+IFX_INTERRUPT(TofIsrHandler, 0, ISR_PRIORITY_CAN_RX);
 void TofIsrHandler(void)
 {
     unsigned int rxID;
     unsigned char rxData[8] = {0,};
     int rxLen;
     Can_RecvMsg(&rxID, rxData, &rxLen);
-    unsigned char dis_status = rxData[3];
-    unsigned short signal_strength = rxData[5] << 8 | rxData[4];
 
-    if (signal_strength != 0) {
-        g_TofValue = rxData[2] << 16 | rxData[1] << 8 | rxData[0];
-//        my_printf("TOF Distance: %d\n", g_TofValue); // for debugging
-    } else {
-//        my_printf("out of range!\n"); // for debugging
-    }
+    g_TofValue = (rxData[2] << 16) | (rxData[1] << 8) | rxData[0];
+    g_DisStatus = rxData[3];
+    g_SignalStrength = (rxData[5] << 8) | rxData[4];
+    g_TofTimestampUs = getTimeUs();
 }
 
-unsigned int Tof_GetValue(void)
+// 안전하게 거리 정보를 가져오는 함수
+void Tof_GetDistance(unsigned int* dist_mm, unsigned short* signal, unsigned char* status, uint64* timestamp_us)
 {
-    return g_TofValue;
+    __disable();
+    *dist_mm = g_TofValue;
+    *signal = g_SignalStrength;
+    *status = g_DisStatus;
+    *timestamp_us = g_TofTimestampUs;
+    __enable();
+}
+
+// 거리 보정 함수: 보정된 거리 값 리턴 (단위: mm)
+unsigned int Tof_GetCorrectedDistance(void)
+{
+    __disable();
+    unsigned int raw = g_TofValue;
+    __enable();
+
+    int corrected = (int)raw - 50; // 50mm 보정
+    if (corrected < 0)
+        corrected = 0;
+    return (unsigned int)corrected;
 }
--- platforms/tc375/BSW/IO/ToF.h
+++ platforms/tc375/BSW/IO/ToF.h
@@ -1,6 +1,8 @@
 #ifndef BSW_IO_TOF_H_
 #define BSW_IO_TOF_H_
 
-unsigned int Tof_GetValue(void);
+//unsigned int Tof_GetValue(void);
+void Tof_GetDistance(unsigned int* dist_mm, unsigned short* signal, unsigned char* status, uint64* timestamp_us);
+unsigned int Tof_GetCorrectedDistance(void);
 
 #endif /* BSW_IO_TOF_H_ */
