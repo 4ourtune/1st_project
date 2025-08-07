7fbbe17c4bba02e828bf8787f32babf9efcdac1b
devcyh <dydgustm@naver.com>
2025-08-07 17:22:23 +0900

    라즈베리파이4 조종기 코드 개선

--- platforms/Raspberry_Pi_4/controller.py
+++ platforms/Raspberry_Pi_4/controller.py
@@ -1,46 +1,63 @@
 import time
-from PySide2.QtCore import *
 from joystick import Joystick
 from bluetooth import BluetoothHandler
 
 
-class Controller(QObject):
-    signal = Signal(int, str)  # int : 0 -> send, 1 -> recv / str : message
-
+class Controller:
     def __init__(self):
-        super().__init__()
         self.js = Joystick()
         self.bt = BluetoothHandler()
         self.bt.register_callback(self.on_bt_message)
         self.running = True
+        self.callback = None  # signal 대신 사용할 콜백 함수
+
+    def set_callback(self, callback_func):
+        """
+        콜백 함수를 설정합니다.
+        callback_func: 함수 (int, str) -> None
+        """
+        self.callback = callback_func
+
+    def emit(self, kind: int, message: str):
+        if self.callback:
+            self.callback(kind, message)
 
     def on_bt_message(self, message: str):
-        self.signal.emit(2, message)
+        self.emit(2, message)
 
     def run(self):
         self.bt.start()
 
-        pre_swt_msg = ""
-        pre_vrx_vry_msg = ""
+        prev_swt = self.js.get_converted_swt()
+        prev_vrx = self.js.get_converted_vrx()
+        prev_vry = self.js.get_converted_vry()
+        last_vr_send_time = time.time()
 
         while self.running:
-            cnt = 0
+            # 스위치 값이 바뀌었을 때만 전송
+            cur_swt = self.js.get_converted_swt()
+            if cur_swt != prev_swt:
+                msg = f"P{cur_swt:02}" + '\0'
+                self.bt.send(msg)
+                self.emit(1, msg)
+                prev_swt = cur_swt
 
-            swt_msg = f"P{self.js.get_converted_swt():02}" + '\0'
-            if swt_msg != pre_swt_msg:
-                self.bt.send(swt_msg)
-                self.signal.emit(1, swt_msg)
-                pre_swt_msg = swt_msg
-                cnt += 1
+            # 조이스틱이 일정 이상 움직였을 때 + 시간 간격도 만족할 때만 전송
+            cur_vrx = self.js.get_converted_vrx()
+            cur_vry = self.js.get_converted_vry()
+            delta_vrx = abs(cur_vrx - prev_vrx)
+            delta_vry = abs(cur_vry - prev_vry)
+            now = time.time()
 
-            vrx_vry_msg = f"M{self.js.get_converted_vrx():02}{self.js.get_converted_vry():02}" + '\0'
-            if vrx_vry_msg != pre_vrx_vry_msg:
-                self.bt.send(vrx_vry_msg)
-                self.signal.emit(1, vrx_vry_msg)
-                pre_vrx_vry_msg = vrx_vry_msg
-                cnt += 1
+            if (delta_vrx >= 2 or delta_vry >= 2) and (now - last_vr_send_time >= 0.1):
+                msg = f"M{cur_vrx:02}{cur_vry:02}" + '\0'
+                self.bt.send(msg)
+                self.emit(1, msg)
+                prev_vrx = cur_vrx
+                prev_vry = cur_vry
+                last_vr_send_time = now
 
-            time.sleep(0.01 * (3 - cnt))
+            time.sleep(0.01)
 
     def stop(self):
         self.running = False
